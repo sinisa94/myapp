e600a71d08608176b8c1b1a42c8c9714556dc976
sinisa94 <sinisa.m1994@gmail.com>
2018-01-01 20:57:40 +0100

    added scss and beutify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Topics from './pages/Topics'
+import './App.css'
 
-const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </nav>
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
-    </div>
-  </Router>
-)
+// var yolo = () => {
+//   var xd="yolo";
+//   window.alert(xd);
+//   return xd;
+// }
+// <Route path="/" component={yolo}/>
+// <li><Link to="/xd">xD</Link></li>
+class App extends Component {
+  render() {
+    return (<Router>
+      <div>
+        <nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+
+        </nav>
+        <Route exact="exact" path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+
+      </div>
+    </Router>)
+  }
+}
+
 export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React  from 'react';
 import '../styles/About.css'
+
 const About = () => (
     <div>
-      <h2>About</h2>
-      <p3>Texta about therehe it goues </p3>
+      <p>Nunc tincidunt facilisis tincidunt. Phasellus dolor augue, dictum nec sapien nec, pellentesque placerat risus. Donec ultricies nisi vel nibh efficitur, nec efficitur velit mollis. Aenean eget laoreet libero. In sit amet vehicula enim. Duis sit amet est at est semper eleifend. Donec scelerisque enim at tempor tempor. Mauris interdum blandit elit, sed varius est. In vitae nulla non dolor condimentum aliquet. Curabitur luctus mi a sem rutrum semper. Integer sollicitudin ante et urna ultricies elementum. Praesent metus nulla, consequat vel volutpat non, lacinia sed nulla.</p>
     </div>
   )
 export default About;
